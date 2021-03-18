d4afaf49f0c8c6b9c168b436ed602fd6491d3834
pjaeyoung <devdodose@gmail.com>
2021-03-18 16:32:40 +0900

    refactor: [6.2] 함수를 없애고 로직을 곧바로 작성하기

--- 06_Basic_Refactoring/inline-function.js
+++ 06_Basic_Refactoring/inline-function.js
@@ -11,6 +11,7 @@ function gatherCustomerData(out, aCustomer) {
 
 export default function reportLinesUpdated(aCustomer) {
   const lines = [];
-  gatherCustomerData(lines, aCustomer);
+  lines.push(["name", aCustomer.name]);
+  lines.push(["location", aCustomer.location]);
   return lines;
 }
