b38642ffb79f89a0978ec70042a8445547563eaa
pjaeyoung <devdodose@gmail.com>
2021-03-18 15:40:38 +0900

    add : babel-jest 설정하여 테스트 시 import 기능을 사용할 수 있게 함

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   preset: "ts-jest",
-  testEnvironment: "jsdom",
+  testEnvironment: "node",
   verbose: true,
   modulePathIgnorePatterns: ["<rootDir>/node_modules/"],
   testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
+  transform: { "\\.[jt]sx?$": "babel-jest" },
 };
