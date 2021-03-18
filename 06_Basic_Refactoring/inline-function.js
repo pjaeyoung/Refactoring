dc8ec454c9f4139b04fa33fb58886d012a02a299
pjaeyoung <devdodose@gmail.com>
2021-03-18 16:27:25 +0900

    add : [6.2] reportLines 리팩토링 전 코드

---
+++ 06_Basic_Refactoring/inline-function.js
@@ -0,0 +1,16 @@
+function reportLines(aCustomer) {
+  const lines = [];
+  gatherCustomerData(lines, aCustomer);
+  return lines;
+}
+
+function gatherCustomerData(out, aCustomer) {
+  out.push(["name", aCustomer.name]);
+  out.push(["location", aCustomer.location]);
+}
+
+export default function reportLinesUpdated(aCustomer) {
+  const lines = [];
+  gatherCustomerData(lines, aCustomer);
+  return lines;
+}
