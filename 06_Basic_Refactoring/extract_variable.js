d95f190891e3db14f10c4df554fcb8300527ef91
pjaeyoung <devdodose@gmail.com>
2021-03-18 17:00:10 +0900

    refactor: [6.3] price 변수 추출하기

--- 06_Basic_Refactoring/extract_variable.js
+++ 06_Basic_Refactoring/extract_variable.js
@@ -8,9 +8,10 @@ function price(order) {
 }
 
 export default function priceUpdated(order) {
-  return (
-    order.quantity * order.itemPrice -
-    Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
-    Math.min(order.quantity * order.itemPrice * 0.1, 100)
-  );
+  const basePrice = order.quantity * order.itemPrice;
+  const quantityDiscount =
+    Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
+  const shipping = Math.min(order.quantity * order.itemPrice * 0.1, 100);
+
+  return basePrice - quantityDiscount + shipping;
 }
