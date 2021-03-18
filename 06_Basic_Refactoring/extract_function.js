8a2cf45558328c30cb404fb1e4855cb96c7a30a1
pjaeyoung <devdodose@gmail.com>
2021-03-18 15:50:47 +0900

    refactor: [6.1] 배너 출력 함수추출하기

--- 06_Basic_Refactoring/extract_function.js
+++ 06_Basic_Refactoring/extract_function.js
@@ -27,3 +27,33 @@ function printOwing(invoice) {
   console.log(`채무액: ${outstanding}`);
   console.log(`마감일: ${invoice.dueDate.toLocaleDateString()}`);
 }
+
+export default function printOwingUpdated(invoice) {
+  let outstanding = 0;
+
+  printBanner();
+
+  // 미해결 채무(outstanding)를 계산한다.
+  for (const o of invoice.order) {
+    outstanding += o.amount;
+  }
+
+  // 마감일(dueDate)을 기록한다.
+  const today = Clock.today;
+  invoice.dueDate = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate() + 30
+  );
+
+  // 세부 사항을 출력한다.
+  console.log(`고객명: ${invoice.customer}`);
+  console.log(`채무액: ${outstanding}`);
+  console.log(`마감일: ${invoice.dueDate.toLocaleDateString()}`);
+
+  function printBanner() {
+    console.log("*************");
+    console.log("****고객 채무****");
+    console.log("*************");
+  }
+}
