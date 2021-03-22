5fc41269706ea10717cfe0af5cb32a5e774ad5d9
pjaeyoung <devdodose@gmail.com>
2021-03-22 19:59:31 +0900

    refactor: [7.3] 리팩토링 후 코드

--- 07_encapsulation/replace_primitive_with_object.js
+++ 07_encapsulation/replace_primitive_with_object.js
@@ -8,3 +8,84 @@ class Order {
 const highPriorityCount = orders.filter(
   (o) => "high" === o.priority || "rush" === o.priority
 ).length;
+
+// 리팩토링 - 1
+class Order {
+  constructor(data) {
+    this._priority = data.priority;
+  }
+
+  get priority() {
+    return this._priority;
+  }
+
+  set priority(newPriority) {
+    this._priority = newPriority;
+  }
+}
+
+// 리팩토링 - 2
+class Priority {
+  constructor(value) {
+    this._value = value;
+  }
+
+  toString() {
+    return this._value;
+  }
+}
+
+class Order {
+  constructor(data) {
+    this._priority = data.priority;
+  }
+
+  get priority() {
+    return this._priority.toString();
+  }
+
+  set priority(newPriority) {
+    this._priority = new Priority(newPriority);
+  }
+}
+
+// 리팩토링 - 3
+class Priority {
+  constructor(value) {
+    if (value instanceof Priority) {
+      return value;
+    } else if (Priority.legalValues().includes(value)) {
+      this._value = value;
+    } else {
+      throw new Error(`<${value}> is invalid for Priority`);
+    }
+  }
+
+  static legalValues() {
+    return ["low", "normal", "high", "rush"];
+  }
+
+  toString() {
+    return this._value;
+  }
+
+  get _index() {
+    return Priority.legalValues().indexOf(this._value);
+  }
+
+  equals(other) {
+    return this._index === other._index;
+  }
+
+  higherThan(other) {
+    return this._index > other._index;
+  }
+
+  lowerThan(other) {
+    return this._index < other._index;
+  }
+}
+
+const highPriorityCount = orders.filter((o) =>
+  o.higherThan(new Priority("normal"))
+).length;
