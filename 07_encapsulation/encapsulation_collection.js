a74736924ae28f277bcfc476d03b177549836c6e
pjaeyoung <devdodose@gmail.com>
2021-03-22 19:22:09 +0900

    add : [7.2] 컬렉션 캡슐화하기 리팩터링 전 코드

---
+++ 07_encapsulation/encapsulation_collection.js
@@ -0,0 +1,35 @@
+// 리팩토링 전
+
+export class Person {
+  constructor(name) {
+    this._name = name;
+    this._courses = [];
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get courses() {
+    return this._courses;
+  }
+
+  set courses(newCourses) {
+    this._courses = newCourses;
+  }
+}
+
+export class Course {
+  constructor(name, isAdvanced) {
+    this._name = name;
+    this._isAdvanced = isAdvanced;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get isAdvanced() {
+    return this._isAdvanced;
+  }
+}
