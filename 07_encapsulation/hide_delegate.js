a417b511c9fd8bac428bfc85a894d8247f2aeafc
pjaeyoung <devdodose@gmail.com>
2021-03-23 22:40:40 +0900

    add : [7.7] 리팩토링 전 코드

---
+++ 07_encapsulation/hide_delegate.js
@@ -0,0 +1,45 @@
+// 리팩토링 전
+
+class Department {
+  constructor(chargeCode, manager) {
+    this._chargeCode = chargeCode;
+    this._manager = manager;
+  }
+
+  get chargeCode() {
+    return this._chargeCode;
+  }
+
+  set chargeCode(newChargeCode) {
+    this._chargeCode = newChargeCode;
+  }
+
+  get manager() {
+    return this._manager;
+  }
+
+  set manager(newManager) {
+    this._manager = newManager;
+  }
+}
+
+class Person {
+  constructor(name, department) {
+    this._name = name;
+    this._deparment = department;
+  }
+  get name() {
+    return this._name;
+  }
+
+  get department() {
+    return this._deparment;
+  }
+
+  set department(newDepartment) {
+    this._deparment = newDepartment;
+  }
+}
+
+const person = new Person("Tony", new Department("2901", "Dyal"));
+const manager = person.department.manager;
