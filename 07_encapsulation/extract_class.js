ccb10723097d027dd5896dfacb1ae53f2a121478
pjaeyoung <devdodose@gmail.com>
2021-03-23 22:10:06 +0900

    refactor: [7.5] 리팩토링 후 코드

--- 07_encapsulation/extract_class.js
+++ 07_encapsulation/extract_class.js
@@ -35,3 +35,67 @@ class Person {
     this._officeNumber = newOfficeNumber;
   }
 }
+
+// 리팩토링 후
+class TelephoneNumber {
+  constructor(areaCode, number) {
+    this._areaCode = areaCode;
+    this._number = number;
+  }
+
+  get toString() {
+    return `${this._areaCode} ${this._number}`;
+  }
+
+  get areaCode() {
+    return this._areaCode;
+  }
+
+  set areaCode(newAreaCode) {
+    this._areaCode = newAreaCode;
+  }
+
+  get number() {
+    return this._number;
+  }
+
+  set number(newNumber) {
+    this._number = newNumber;
+  }
+}
+class Person {
+  constructor(name, officeAreaCode, officeNumber) {
+    this._name = name;
+    this._officeAreaCode = officeAreaCode;
+    this._officeNumber = officeNumber;
+    this._telephoneNumber = new TelephoneNumber(officeAreaCode, officeNumber);
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    this._name = newName;
+  }
+
+  get telephoneNumber() {
+    return this._telephoneNumber.toString();
+  }
+
+  get officeAreaCode() {
+    return this._telephoneNumber.areaCode;
+  }
+
+  set officeAreaCode(newOfficeAreaCode) {
+    this._telephoneNumber.areaCode = newOfficeAreaCode;
+  }
+
+  get officeNumber() {
+    return this._telephoneNumber.number;
+  }
+
+  set officeNumber(newOfficeNumber) {
+    this._telephoneNumber.number = newOfficeNumber;
+  }
+}
