71be894940b4f07e74f61e317f9805c59d99658a
pjaeyoung <devdodose@gmail.com>
2021-03-23 21:46:59 +0900

    refactor: [7.4] Order class price 메서드  리팩토링

--- query.js
+++ query.js
@@ -26,14 +26,20 @@ export default class OrderUpdated {
     this._item = item;
   }
 
-  get price() {
-    const basePrice = this._quantitiy * this._item.price;
+  get basePrice() {
+    return this._quantitiy * this._item.price;
+  }
+
+  get discountFactor() {
     let discountFactor = 0.98;
 
-    if (basePrice > 1000) {
+    if (this.basePrice > 1000) {
       discountFactor -= 0.03;
     }
+    return discountFactor;
+  }
 
-    return basePrice * discountFactor;
+  get price() {
+    return this.basePrice * this.discountFactor;
   }
 }
