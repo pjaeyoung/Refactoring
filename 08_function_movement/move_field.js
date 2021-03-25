55322f7f6a725a691448803e5a3e0f3f0bf210a0
pjaeyoung <devdodose@gmail.com>
2021-03-25 18:14:13 +0900

    refactor: [8.2]

--- 08_function_movement/move_field.js
+++ 08_function_movement/move_field.js
@@ -24,3 +24,74 @@ class CustomerContract {
     this._startDate = startDate;
   }
 }
+
+// 리팩토링 - 1
+class Customer {
+  constructor(name, discountRate) {
+    this._name = name;
+    this._discountRate = discountRate;
+    this._contract = new CustomerContract(dateToday(), discountRate);
+  }
+
+  get discountRate() {
+    return this._contract.discountRate;
+  }
+
+  becomePreffered() {
+    this._contract.discountRate += 0.03;
+  }
+
+  applyDiscount(amout) {
+    return amount.subtract(amount.multiply(this._contract.discountRate));
+  }
+}
+
+class CustomerContract {
+  constructor(startDate, discountRate) {
+    this._startDate = startDate;
+    this._discountRate = discountRate;
+  }
+
+  get discountRate() {
+    return this._discountRate;
+  }
+
+  set discountRate(newDiscountRate) {
+    this._discountRate = newDiscountRate;
+  }
+}
+
+// 리팩토링 - 2
+class Customer {
+  constructor(name, discountRate) {
+    this._name = name;
+    this._contract = new CustomerContract(dateToday(), discountRate);
+  }
+
+  get discountRate() {
+    return this._contract.discountRate;
+  }
+
+  becomePreffered() {
+    this._contract.discountRate += 0.03;
+  }
+
+  applyDiscount(amout) {
+    return amount.subtract(amount.multiply(this._contract.discountRate));
+  }
+}
+
+class CustomerContract {
+  constructor(startDate, discountRate) {
+    this._startDate = startDate;
+    this._discountRate = discountRate;
+  }
+
+  get discountRate() {
+    return this._discountRate;
+  }
+
+  set discountRate(newDiscountRate) {
+    this._discountRate = newDiscountRate;
+  }
+}
