2b5aaf5535082e2dcc73b41ea44c3ff5a49676e5
pjaeyoung <devdodose@gmail.com>
2021-03-25 17:52:49 +0900

    refactor: [8.1]

--- 08_function_movement/move_function.js
+++ 08_function_movement/move_function.js
@@ -23,3 +23,121 @@ function trackSummary(points) {
   function distance(p1, p2) {}
   function calculateTime() {}
 }
+
+// 리팩토링 - 1
+function trackSummary(points) {
+  const totalTime = calculateTime();
+  const totalDistance = calculateDistance();
+  const pace = totalTime / 60 / totalDistance;
+
+  return {
+    time: totalTime,
+    distance: totalDistance,
+    pace,
+  };
+
+  function calculateDistance() {
+    // --> 소스 함수
+    let result = 0;
+    for (let i = 1; i < points.length; i++) {
+      result += distance(points[i - 1], points[i]);
+    }
+    return result;
+  }
+
+  function distance(p1, p2) {}
+  function calculateTime() {}
+}
+
+function top_calculateDistance() {
+  let result = 0;
+  for (let i = 1; i < points.length; i++) {
+    result += distance(points[i - 1], points[i]);
+  }
+  return result;
+}
+
+// 리팩토링 - 2
+function trackSummary(points) {
+  const totalTime = calculateTime();
+  const totalDistance = calculateDistance();
+  const pace = totalTime / 60 / totalDistance;
+
+  return {
+    time: totalTime,
+    distance: totalDistance,
+    pace,
+  };
+
+  function calculateDistance() {
+    // --> 소스 함수
+    let result = 0;
+    for (let i = 1; i < points.length; i++) {
+      result += distance(points[i - 1], points[i]);
+    }
+    return result;
+    function distance(p1, p2) {} // 연관된 함수 옮기기
+  }
+  function calculateTime() {}
+}
+
+function top_calculateDistance(points) {
+  // 인자로 points 접근
+  let result = 0;
+  for (let i = 1; i < points.length; i++) {
+    result += distance(points[i - 1], points[i]);
+  }
+  return result;
+  function distance(p1, p2) {} // 연관된 함수 옮기기
+}
+
+// 리팩토링 - 3
+function trackSummary(points) {
+  const totalTime = calculateTime();
+  const totalDistance = calculateDistance();
+  const pace = totalTime / 60 / totalDistance;
+
+  return {
+    time: totalTime,
+    distance: totalDistance,
+    pace,
+  };
+
+  function calculateDistance() {
+    return top_calculateDistance(points);
+  }
+
+  function calculateTime() {}
+}
+
+function top_calculateDistance(points) {
+  let result = 0;
+  for (let i = 1; i < points.length; i++) {
+    result += distance(points[i - 1], points[i]);
+  }
+  return result;
+  function distance(p1, p2) {}
+}
+
+// 리팩토링 - 4
+function trackSummary(points) {
+  const totalTime = calculateTime();
+  const pace = totalTime / 60 / totalDistance(points);
+
+  return {
+    time: totalTime,
+    distance: totalDistance(points),
+    pace,
+  };
+
+  function calculateTime() {}
+}
+
+function totalDistance(points) {
+  let result = 0;
+  for (let i = 1; i < points.length; i++) {
+    result += distance(points[i - 1], points[i]);
+  }
+  return result;
+  function distance(p1, p2) {}
+}
