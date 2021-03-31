ded22baaea5dfc0acfd390f918644a304a3890a7
pjaeyoung <devdodose@gmail.com>
2021-03-31 10:22:59 +0900

    refactor:  totalSalary 계산과 youngest 계산  반복문 쪼개기

--- 08_function_movement/split_loop.js
+++ 08_function_movement/split_loop.js
@@ -3,10 +3,13 @@ export function printYoungestAndTotalSalary(people) {
   let totalSalary = 0;
 
   for (const p of people) {
+    totalSalary += p.salary;
+  }
+
+  for (const p of people) {
     if (p.age < youngest) {
       youngest = p.age;
     }
-    totalSalary += p.salary;
   }
 
   return `최연소: ${youngest}, 총 급여: ${totalSalary}`;
