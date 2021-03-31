f52b80d12e38053d7c238fb0264a187bfa626126
pjaeyoung <devdodose@gmail.com>
2021-03-31 10:29:08 +0900

    refactor: getYoungest 함수 추출하기

--- 08_function_movement/split_loop.js
+++ 08_function_movement/split_loop.js
@@ -6,7 +6,9 @@ export function printYoungestAndTotalSalary(people) {
     }
   }
 
-  return `최연소: ${youngest}, 총 급여: ${calculateTotalSalary(people)}`;
+  return `최연소: ${getYoungest(people)}, 총 급여: ${calculateTotalSalary(
+    people
+  )}`;
 
   function calculateTotalSalary(people) {
     let result = 0;
@@ -14,5 +16,15 @@ export function printYoungestAndTotalSalary(people) {
       result += p.salary;
     }
     return result;
+  }
+
+  function getYoungest(people) {
+    let youngest = people[0] ? people[0].age : Infinity;
+    for (const p of people) {
+      if (p.age < youngest) {
+        youngest = p.age;
+      }
+    }
+    return youngest;
   }
 }
