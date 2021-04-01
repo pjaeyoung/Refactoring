371c0f48221bba08de965317a48b2dac12effd9e
pjaeyoung <devdodose@gmail.com>
2021-04-01 16:03:56 +0900

    refactor : [8.8] filter 파이프라인 사용하여 빈줄 지우기

--- replace_loop_with_pipeline.js
+++ replace_loop_with_pipeline.js
@@ -2,11 +2,9 @@ export function acquireACountryData(input, country) {
   const lines = input.split("\n");
   const result = [];
 
-  const loopItems = lines.slice(1);
-  console.log(loopItems);
+  const loopItems = lines.slice(1).filter((aLine) => aLine.trim() !== "");
 
   for (const line of loopItems) {
-    if (line.trim() === "") continue;
     const record = line.split(",");
     if (record[1].trim() === country) {
       result.push({
