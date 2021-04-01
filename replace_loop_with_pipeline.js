fb6f7cdf2cac30867e611afaac8b78ac11e6d761
pjaeyoung <devdodose@gmail.com>
2021-04-01 16:14:34 +0900

    refactor : [8.8] 변수명 바꾸기

--- replace_loop_with_pipeline.js
+++ replace_loop_with_pipeline.js
@@ -4,6 +4,6 @@ export function acquireACountryData(input, country) {
     .slice(1)
     .filter((aLine) => aLine.trim() !== "")
     .map((aLine) => aLine.split(","))
-    .filter((record) => record[1].trim() === country)
-    .map((record) => ({ city: record[0].trim(), phone: record[2].trim() }));
+    .filter((fields) => fields[1].trim() === country)
+    .map((fields) => ({ city: fields[0].trim(), phone: fields[2].trim() }));
 }
