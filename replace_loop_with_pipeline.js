f761e3884b45a6e7d73772044d86a0eae33857cf
pjaeyoung <devdodose@gmail.com>
2021-04-01 16:12:48 +0900

    refactor : [8.8] 죽은 코드 없애기

--- replace_loop_with_pipeline.js
+++ replace_loop_with_pipeline.js
@@ -1,17 +1,11 @@
 export function acquireACountryData(input, country) {
   const lines = input.split("\n");
-  const result = [];
-
-  const loopItems = lines
+  const result = lines
     .slice(1)
     .filter((aLine) => aLine.trim() !== "")
     .map((aLine) => aLine.split(","))
     .filter((record) => record[1].trim() === country)
     .map((record) => ({ city: record[0].trim(), phone: record[2].trim() }));
-
-  for (const record of loopItems) {
-    result.push(record);
-  }
 
   return result;
 }
