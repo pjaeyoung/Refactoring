94add41f79337df4906d016550401f2915bbfe6b
pjaeyoung <devdodose@gmail.com>
2021-04-01 16:09:10 +0900

    refactor: [8.8] filter 파이프라인을 사용하여 country와 일치하는 레코드만 가져오기

--- replace_loop_with_pipeline.js
+++ replace_loop_with_pipeline.js
@@ -5,15 +5,14 @@ export function acquireACountryData(input, country) {
   const loopItems = lines
     .slice(1)
     .filter((aLine) => aLine.trim() !== "")
-    .map((aLine) => aLine.split(","));
+    .map((aLine) => aLine.split(","))
+    .filter((record) => record[1].trim() === country);
 
   for (const record of loopItems) {
-    if (record[1].trim() === country) {
-      result.push({
-        city: record[0].trim(),
-        phone: record[2].trim(),
-      });
-    }
+    result.push({
+      city: record[0].trim(),
+      phone: record[2].trim(),
+    });
   }
 
   return result;
