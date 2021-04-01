acc61f1f226c0b8137c4c17169f3ec02d016ac77
pjaeyoung <devdodose@gmail.com>
2021-04-01 15:42:52 +0900

    move csv variable to test code

--- replace_loop_with_pipeline.js
+++ replace_loop_with_pipeline.js
@@ -1,10 +1,3 @@
-const csv = `office, country, telephone
-chicago, USA, +1 312 373 1000
-Beiging, China, +86 4008 900 505,
-Bangalore, India, +91 80 4064 9570
-Porto Alegre, Brazil, +55 51 3079 3550
-Chennai, India, +91 44 660 44766`;
-
 export function acquireACountryData(input, country) {
   const lines = input.split("\n");
   let firstLine = true;
