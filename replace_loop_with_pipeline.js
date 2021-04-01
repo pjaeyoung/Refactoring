5535c1ba23460823bc54689bf17687d491b8bb1b
pjaeyoung <devdodose@gmail.com>
2021-04-01 16:13:52 +0900

    refactor: [8.8] 변수 인라인하여 반환하기

--- replace_loop_with_pipeline.js
+++ replace_loop_with_pipeline.js
@@ -1,11 +1,9 @@
 export function acquireACountryData(input, country) {
   const lines = input.split("\n");
-  const result = lines
+  return lines
     .slice(1)
     .filter((aLine) => aLine.trim() !== "")
     .map((aLine) => aLine.split(","))
     .filter((record) => record[1].trim() === country)
     .map((record) => ({ city: record[0].trim(), phone: record[2].trim() }));
-
-  return result;
 }
